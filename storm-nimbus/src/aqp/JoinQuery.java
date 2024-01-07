4820d872f3e6f938d874ffd6d0ce5fcab3b04b01
pranayat <pranayatomar@gmail.com>
2024-01-07 18:21:42 +0100

    implement n-way spatial joins across n streams

--- storm-nimbus/src/aqp/JoinQuery.java
+++ storm-nimbus/src/aqp/JoinQuery.java
@@ -5,12 +5,19 @@ import org.apache.storm.tuple.Tuple;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class JoinQuery {
     int radius;
     List<String> streamIds;
     List<String> fields;
     List<List<Tuple>> results;
+
+    enum QuerySpherePosition {
+        INSIDE,
+        INTERSECTS,
+        OUTSIDE
+    }
 
     public JoinQuery(int radius, List<String> streamIds, List<String> fields) {
         Collections.sort(fields);
@@ -33,7 +40,82 @@ public class JoinQuery {
         return this.fields;
     }
 
-    public void addResult(List<Tuple> result) {
+    private QuerySpherePosition getQuerySpherePosition(double queryTupleToCentroidDistance, double clusterRadius) {
+        if (queryTupleToCentroidDistance < clusterRadius) {
+            return QuerySpherePosition.INSIDE;
+        } else if (queryTupleToCentroidDistance < clusterRadius + this.getRadius()) {
+            return QuerySpherePosition.INTERSECTS;
+        } else {
+            return QuerySpherePosition.INTERSECTS;
+        }
+    }
+
+    private List<Tuple> findJoinPartnersInStream(Tuple tuple, Grid grid, String streamToJoin) {
+        BPlusTree bPlusTree = grid.getBPlusTree();
+        TupleWrapper tupleWrapper = new TupleWrapper(grid.getAxisNamesSorted());
+        Distance distance = new EuclideanDistance();
+        List<Tuple> joinCandidates = new ArrayList<>();
+
+        for (Cluster cluster : grid.getClusters()) {
+            double queryTupleToCentroidDistance = distance.calculate(cluster.getCentroid(), tupleWrapper.getCoordinates(tuple));
+
+            switch (this.getQuerySpherePosition(queryTupleToCentroidDistance, cluster.getRadius())) {
+                case INSIDE:
+                    joinCandidates.addAll(bPlusTree.search(cluster.getI() * grid.getC() + queryTupleToCentroidDistance - this.getRadius(),
+                            Math.min(cluster.getI() * grid.getC() + cluster.getRadius(), cluster.getI() * grid.getC() + queryTupleToCentroidDistance + this.getRadius())));
+                case INTERSECTS:
+                    joinCandidates.addAll(bPlusTree.search(cluster.getI() * grid.getC() + queryTupleToCentroidDistance - this.getRadius(),
+                            cluster.getI() * grid.getC() + cluster.getRadius()));
+            }
+        }
+
+        // remove join partners found in the same stream to prevent within stream joins
+        List<Tuple> joinCandidatesFromOtherStreams = new ArrayList<>();
+        for (Tuple joinCandidate : joinCandidates) {
+            if (streamToJoin.equals(joinCandidate.getSourceStreamId())) {
+                joinCandidatesFromOtherStreams.add(joinCandidate);
+            }
+        }
+
+        return joinCandidatesFromOtherStreams;
+    }
+
+    public void execute(Tuple tuple, Grid grid) {
+        // remove tuple's stream to prevent joins within streams
+        List<String> unjoinedStreams = new ArrayList<>();
+        unjoinedStreams.addAll(this.streamIds.stream()
+                .filter(s -> !s.equals(tuple.getSourceStreamId()))
+                .collect(Collectors.toList()));
+
+        List<Tuple> leftTuplesToJoin = new ArrayList<>();
+        leftTuplesToJoin.add(tuple);
+        // initialized with [A1], find partners for [A1] in stream B
+        while (unjoinedStreams.size() > 0) {
+            // join stream C now (assuming already joined [A1] with stream B in previous loop to get [A1, B1, B2])
+            String streamToJoin = unjoinedStreams.remove(unjoinedStreams.size() - 1);
+            List<Tuple> partnerTuples = new ArrayList<>();
+            // [A1, B1, B2] will be joined with tuples in stream C
+            for (Tuple leftTuple : leftTuplesToJoin) {
+                // we found [C1, C2, C3, C4] join partners for [A1, B1, B2]
+                List<Tuple> joinPartners = this.findJoinPartnersInStream(leftTuple, grid, streamToJoin);
+                // every single tuple [A1, B1, B2] should find a join partner in stream C, else return
+                if (joinPartners.size() == 0) {
+                    return;
+                }
+                partnerTuples.addAll(joinPartners);
+            }
+
+            // these join results will now be joined with the remaining streams
+            // eg. [A1, B1, B2, B3] will join tuples from stream C next
+            leftTuplesToJoin.addAll(partnerTuples);
+        }
+
+
+        List<Tuple> result = new ArrayList<>();
+        result.addAll(leftTuplesToJoin);
+
+        // we found join partners [B1, B2, B3, C1, C2, D1, D2, D3, D4] for tuple A1
+        // add [A1, B1, B2, B3, C1, C2, D1, D2, D3, D4] to query result
         this.results.add(result);
     }
 }
