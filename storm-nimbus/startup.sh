5162116bb5dd0f387f5e63a37be2c453f80ddda3
pranayat <pranayatomar@gmail.com>
2024-02-14 15:18:00 +0100

    clean startup.sh to no start supervisor, nimbus and ui processes in local mode

--- storm-nimbus/startup.sh
+++ storm-nimbus/startup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo "STARTING UP IN LOCAL MODE"
+echo "mvn package to build jar"
+echo "storm local target/aqp-2.5.0.jar aqp.AqpTopology to start local topology"
+# could also do the above to in the script here, but doing it manually as we don't have to build everytime
 set -x
-storm nimbus &
-storm dev-zookeeper &
-storm supervisor &
-storm ui
+sleep infinity # so container won't exit
